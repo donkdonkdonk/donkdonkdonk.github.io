ff739f7ad34bce5ad475d75788dee0695ff7e4d4
donkdonkdonk <aldona97@windowslive.com>
2018-07-13 10:24:06 +0200

    donk

---
+++ list.js
@@ -0,0 +1,50 @@
+(function($){
+  $.fn.addX = function() {
+    this.append('<span class="close">\u00D7 </span>');
+  }
+})(jQuery);
+
+let close = document.getElementsByClassName("close");
+for (let i = 0; i < close.length; i++) {
+
+}
+
+
+$("document").ready(function(){
+
+  $("li").each(function(){
+    $(this).addX();
+    console.log(this);
+  })
+
+  $(".addButton").click(function(){
+    let toAdd = $("#inputActivity").val();
+    if (toAdd == '')
+      alert("Wprowadz nazwe aktywnosci!");
+      else {
+    $("#toDoList").append('<li class="">' + toAdd + '</li>');
+    $("#inputActivity").val('');
+    $("li:last").addX();
+  }
+  })
+
+
+
+ $(document).on('click', '.close', function(){
+   $(this).parent().fadeOut(1000);
+ })
+
+ $(document).on('click', 'li', function(){
+     $(this).toggleClass("done");
+     let newPlace = $("li:last");
+     if($(this).hasClass("done"))
+        $(this).insertAfter(newPlace);
+
+ })
+
+ $('ul').sortable({
+   helper: 'clone'
+ });
+
+
+})
