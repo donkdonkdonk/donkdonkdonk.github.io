3ea8ecc95b86fcc7a8482598696ffc6a51b4b20b
donkdonkdonk <aldona97@windowslive.com>
2018-07-13 10:28:30 +0200

    upload

--- list.js
+++ list.js
@@ -1,4 +1,4 @@
-(function($){
+(function($) {
   $.fn.addX = function() {
     this.append('<span class="close">\u00D7 </span>');
   }
@@ -10,41 +10,41 @@ for (let i = 0; i < close.length; i++) {
 }
 
 
-$("document").ready(function(){
+$("document").ready(function() {
 
-  $("li").each(function(){
+  $("li").each(function() {
     $(this).addX();
     console.log(this);
   })
 
-  $(".addButton").click(function(){
+  $(".addButton").click(function() {
     let toAdd = $("#inputActivity").val();
     if (toAdd == '')
       alert("Wprowadz nazwe aktywnosci!");
-      else {
-    $("#toDoList").append('<li class="">' + toAdd + '</li>');
-    $("#inputActivity").val('');
-    $("li:last").addX();
-  }
+    else {
+      $("#toDoList").append('<li class="">' + toAdd + '</li>');
+      $("#inputActivity").val('');
+      $("li:last").addX();
+    }
   })
 
 
 
- $(document).on('click', '.close', function(){
-   $(this).parent().fadeOut(1000);
- })
+  $(document).on('click', '.close', function() {
+    $(this).parent().fadeOut(1000);
+  })
 
- $(document).on('click', 'li', function(){
-     $(this).toggleClass("done");
-     let newPlace = $("li:last");
-     if($(this).hasClass("done"))
-        $(this).insertAfter(newPlace);
+  $(document).on('click', 'li', function() {
+    $(this).toggleClass("done");
+    let newPlace = $("li:last");
+    if ($(this).hasClass("done"))
+      $(this).insertAfter(newPlace);
 
- })
+  })
 
- $('ul').sortable({
-   helper: 'clone'
- });
+  $('ul').sortable({
+    helper: 'clone'
+  });
 
 
 })
